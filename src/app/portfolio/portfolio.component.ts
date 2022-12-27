9717b1d01e1b67646b32919b7b4beb31bdf709b1
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-12-27 12:58:30 -0600

    working on portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,61 +12,76 @@ export class PortfolioComponent {
   
   projectList = [
     {
+      name: 'Tangy Web',
+      technologies: 'ASP.NET Blazor, Azure',
+      description: 'Robust full-stack E-Commerce site built from Bhrugen Patels Udemy Blazor Bootcamp. Has a customor-facing client application with Stripe payment, shopping cart, checkout, and confirmation. Also a Seller inventory management Server project to CRUD stock',
+      image: '../../../assets/projectScreenshots/TangyClient.png',
+      github: 'https://github.com/shalant/Tangy',
+      deployment: 'https://tangywebclient11.azurewebsites.net/'
+    },
+    {
       name: 'ATA Airlines',
       technologies: 'ASP.NET Web API, Angular, Azure',
       description: 'This is a full-stack Airline application, with an Angular front end, ASP.NET back end, and configured to use a MS SQL Server DB. The admin can add Passenger, Flights, and create a booking',
       image: '../../../assets/projectScreenshots/ATA.png',
-      github: 'https://github.com/shalant/AirlineTravelApp'
+      github: 'https://github.com/shalant/AirlineTravelApp',
+      deployment: ''
     },
     {
       name: 'Tiger Phone',
       technologies: 'ASP.NET Web API, Angular, Azure',
       description: 'This is a full-stack Airline application, with an Angular front end, ASP.NET back end, and configured to use a MS SQL Server DB. The admin can add Passenger, Flights, and create a booking',
       image: '../../../assets/projectScreenshots/Tiger.png',
-      github: 'https://github.com/shalant/TigerPhone'
+      github: 'https://github.com/shalant/TigerPhone',
+      deployment: ''
     },
     {
       name: 'Doug Rosenberg Music',
       technologies: 'Javascript, HTML, CSS- Bootstrap, ParticlesJS, Text effects, Chatbot, Photo Gallery Carousel',
       description: 'Homepage for my music universe, with sheet music for students and links to buy CDs',
       image: '../../../assets/projectScreenshots/DRM.png',
-      github: 'https://github.com/shalant/music-site-2'
+      github: 'https://github.com/shalant/music-site-2',
+      deployment: ''
     },
     {
       name: 'AWS Resilient Fault Tolerant Wordpress',
       technologies: 'AWS, CloudFormation, Elasticache, Aurora, EFS, ALB, Auto-scaling',
       description: 'The AWS Architecture Capstone Project involves hosting a WordPress site on a cloud environment, using several key principles',
       image: '../../../assets/projectScreenshots/FancyAwsBlog.png',
-      github: 'https://github.com/shalant/AWS-resilient-fault-tolerant-wordpress'
+      github: 'https://github.com/shalant/AWS-resilient-fault-tolerant-wordpress',
+      deployment: ''
     },
     {
       name: 'Dynamic Wines',
       technologies: 'React, Google Maps, Express, Node, Mongo DB',
       description: 'Final group project for a class on the MERN stack',
       image: '../../../assets/projectScreenshots/DynamicWines.png',
-      github: 'https://github.com/shalant/dynamicwines2/'
-
+      github: 'https://github.com/shalant/dynamicwines2/',
+      deployment: ''
     },
     {
       name: 'CI/CD Pipeline',
       technologies: 'Jenkins, AWS, Docker, Kubernetes, Github',
       description: 'blahblahblah',
       image: '../../../assets/projectScreenshots/CICD.png',
-      github: 'https://github.com/shalant/AirlineTravelApp'
+      github: 'https://github.com/shalant/AirlineTravelApp',
+      deployment: ''
     },
     {
       name: 'Blah',
       technologies: 'blah',
       description: 'blahblahblah',
       image: '../../../assets/projectScreenshots/ATA.png',
-      github: 'https://github.com/shalant/AirlineTravelApp'
+      github: 'https://github.com/shalant/AirlineTravelApp',
+      deployment: ''
     },
     {
       name: 'Blah',
       technologies: 'blah',
       description: 'blahblahblah',
       image: '../../../assets/projectScreenshots/ATA.png',
-      github: 'https://github.com/shalant/AirlineTravelApp'
+      github: 'https://github.com/shalant/AirlineTravelApp',
+      deployment: ''
     },
   ]
 
